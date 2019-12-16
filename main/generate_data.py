f123b6e17e56385571e76588a138986ff7fc4d37
Administrator <admin@example.com>
2019-12-16 23:27:18 +0400

    sex - М/Ж

--- main/generate_data.py
+++ main/generate_data.py
@@ -1,4 +1,4 @@
-from .models import Race, Seller, Buyer, Vital
+from .models import Race, Seller, Buyer, Vital, Sex
 
 
 def generate_data():
@@ -7,3 +7,5 @@ def generate_data():
     seller, _ = Seller.objects.get_or_create(name="Джанго Освобождённый", phone=911)
     buyer, _ = Buyer.objects.get_or_create(name="Сатана", phone=666)
     vital, _ = Vital.objects.get_or_create(name="Мозг")
+    sex1, _ = Sex.objects.get_or_create(name="М")
+    sex2, _ = Sex.objects.get_or_create(name="Ж")
