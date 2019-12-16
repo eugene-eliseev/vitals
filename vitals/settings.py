2f2cdc29c4565f4780bcf5cc1cc2549e426a9712
GreyOwl <Bruss.serials@gmail.com>
2019-12-16 22:48:48 +0400

    Success page

--- vitals/settings.py
+++ vitals/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
