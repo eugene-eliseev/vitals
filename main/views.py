b9e2de59e03c73c897e75c8f1abae2ec3b9bbae2
Administrator <admin@example.com>
2019-12-16 22:44:54 +0400

    добавлены страницы инфы о человеке и список людей + соответствующие вьюхи

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 from .generate_data import generate_data
 from .models import People, Vital, Race, PeopleVital, Seller, Buyer
 from .forms import PeopleForm, RaceForm, VitalForm, PeopleVitalForm, SellerForm, BuyerForm
@@ -62,3 +62,13 @@ class PeopleVitalCreateView(CreateView):
 def index(request):
     generate_data()
     return render(request, "index.html")
+
+
+class PeopleList(ListView):
+    model = People
+    template_name = 'people_list.html'
+
+
+class PeopleDetail(DetailView):
+    model = People
+    template_name = 'people_detail.html'
